a9acb37376242c4dc5efe04c768a5383e6a1b401
AfifaYousaf <143204158+AfifaYousaf@users.noreply.github.com>
2026-09-08 20:53:20 -0500

    Update input.hpp

--- energyAnalysis/input.hpp
+++ energyAnalysis/input.hpp
@@ -21,6 +21,7 @@
 /************************************************* 
  *  String for docopt
 *************************************************/ 
+// Change #1: add new command-line options to the USAGE string
 static const char USAGE[] =
 R"(EnergyAnalysis.
 
@@ -32,6 +33,10 @@ R"(EnergyAnalysis.
   Options:
     -h --help    Show this screen.
      --version   Show version.
+     --ftype=<ftype> Pair-point construction type: all, cross, or nocross [default: all].
+     --dist-min=<dmin> Minimum pair-point distance cutoff, a.u. [default: 1.0].
+     --dist-max=<dmax> Maximum pair-point distance cutoff, a.u. [default: 3.0].
+
 )";
 
 
@@ -62,15 +67,28 @@ int parse_input(const std::map<std::string,docopt::value> args,
        // if (arg.first == "--grid-path") {jobinfo.set_grid_path(arg.second.asString());}
        // if (arg.first == "--orb-path")  {jobinfo.set_orb_path(arg.second.asString());}
        // if (arg.first == "--points-sets-path") {jobinfo.set_points_set_path(arg.second.asString());}
-
-
+      
+       // Change #2: parse --ftype, --dist-min, --dist-max, and call set_coords_path()
+      // PAIR-POINT EXTENSION options
+        if (arg.first == "--ftype" && arg.second)
+        {
+            std::string ft = arg.second.asString();
+            if  (ft == "all"  jobinfo.ftype = jobinfo::Ftype::all;
+            else if (ft == "cross"  jobinfo.ftype = jobinfo::Ftype::cross;
+            else if (ft == "nocross"  jobinfo.ftype = jobinfo::Ftype::nocross;
+            else { printf("Bad --ftype value: %s\n", ft.c_str()); exit(1); }
+       }
+       if (arg.first == "--dist-min" && arg.second) {jobinfo.pair_dist_min = std::stod(arg.second.asString());}
+       if (arg.first == "--dist-max" && arg.second) {jobinfo.pair_dist_max = std::stod(arg.second.asString());}
+  
+       
        //Seed options
        // if (arg.first == "--seed" && arg.second) {jobinfo.seed = (int) arg.second.asLong();}
-
        // set relative path.
        jobinfo.set_work_path();
        jobinfo.set_orb_path();
        jobinfo.set_grid_path();
+       jobinfo.set_coords_path(); //change
        jobinfo.set_t_path();
 
     }//end loop over arguements
